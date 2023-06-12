36c951a40fb74d7a61a6cf9da6920ac01fdb9d45
Daniel Espinosa <103462628+danessti@users.noreply.github.com>
2023-06-12 16:38:44 -0500

    cambios

--- src/Calculo/Main.java
+++ src/Calculo/Main.java
@@ -8,14 +8,14 @@ public class Main {
         Triangulo trian = new Triangulo(3, 4);
         Circulo circu = new Circulo(4);
 
-        System.out.println(cuadro.calcularArea());
-        System.out.println(cuadro.calcularPerimetro() + "\n");
+        System.out.println("El area del cuadrado es " + cuadro.calcularArea());
+        System.out.println("El perimetro del cuadrado es " + cuadro.calcularPerimetro() + "\n");
 
-        System.out.println(trian.calcularArea());
-        System.out.println(trian.calcularPerimetro() + "\n");
+        System.out.println("El area del triangulo es " + trian.calcularArea());
+        System.out.println("El perimetro del triangulo es " + trian.calcularPerimetro() + "\n");
 
-        System.out.println(circu.calcularArea());
-        System.out.println(circu.calcularPerimetro() + "\n");
+        System.out.println("El area del circulo es " + circu.calcularArea());
+        System.out.println("El perimetro del circulo es " + circu.calcularPerimetro() + "\n");
 
     }
 
